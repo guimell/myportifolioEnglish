16b39c3f1da23be4be204f6dc5292dcf4f793e83
Davi Jorge <guimell11@hotmail.com>
2023-02-10 12:54:40 -0300

    added sections for profile and projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,12 +47,7 @@ export default function Home() {
           <p className={inter.className}>Conheça todos os meu projetos.</p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="#contact" className={styles.card}>
           <h2 className={inter.className}>
             Contato <span>-&gt;</span>
           </h2>
@@ -60,13 +55,58 @@ export default function Home() {
         </a>
       </div>
       <div id="aboutMe" className={styles.aboutMe}>
-        <h2>About me</h2>
+        {/* profile image and data */}
+        <div className={styles.profile}>
+          <div className={styles.profileImage}>
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.image}
+              width={100}
+              height={100}
+              priority
+            />
+          </div>
+          <h2>Davi Guimell</h2>
+          {/* skill */}
+          <div className="inline">
+            <div className={styles.skills}>
+              <ul>
+                <li>HTML & CSS</li>
+                <li>NEXT.JS</li>
+                <li>DART-FLUTTER</li>
+              </ul>
+            </div>
+            <div className={styles.skills}>
+              <ul>
+                <li>JAVA</li>
+                <li>JAVASCRIPT</li>
+                <li>PYTHON</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={styles.verticalLine}></div>
+        {/* my goals */}
+        <div className={styles.profileDescription}>
+          <h2>Sobre mim:</h2>
+          <br />
+          <p>
+            Realizarei o trabalho que me for passado com virtuosidade,
+            ambicionando crescimento e desenvolvimento pessoal na área que
+            escolhi, visando sempre uma boa comunicação com empatia,
+            criatividade, abertura para inovações e gestão de projetos.
+          </p>
+        </div>
       </div>
+      {/* my projects */}
       <div id="portifolio" className={styles.portifolio}>
         <div className={styles.projects}>x</div>
         <div className={styles.projects}>y</div>
         <div className={styles.projects}>z</div>
       </div>
+      {/* contact informations */}
+      <div id="contact"></div>
     </main>
   );
 }
