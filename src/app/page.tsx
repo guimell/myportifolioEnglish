7a36873488e60fe06d689aa98b2778c5e848489a
Davi Jorge <guimell11@hotmail.com>
2023-02-15 23:45:29 -0300

    added components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,16 @@ import { Inter } from "@next/font/google";
 import About from "components/about";
 import Projects from "components/projects";
 import Contact from "components/contact";
+import Education from "components/education";
+import Experience from "components/experience";
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
       <Title />
       <About />
+      <Experience />
+      <Education />
       <Projects />
       <Contact />
       <div id="contact"></div>
