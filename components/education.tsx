1860d422dd5b1a025e1fc02feb72d6429173a88b
Davi Jorge <guimell11@hotmail.com>
2023-02-17 00:54:12 -0300

    changed the type of the file

--- components/education.tsx
+++ components/education.tsx
@@ -7,14 +7,14 @@ const Education = () => {
     <div id="education" className="section">
       <h1 className="title">Educação</h1>
       <div className={styles.educationGrid}>
-        <div className={styles.educationContainer}>
+        <div className={`${styles.educationContainer} reveal`}>
           <h1>UNIFACS - Universidade Salvador</h1>
           <br />
           <li>Conclusão ensino superior - Ciências da computação</li>
           <br />
           <li>2019-2023</li>
         </div>
-        <div className={styles.educationContainer}>
+        <div className={`${styles.educationContainer} reveal`}>
           <h1>Fundação Bradesco</h1>
           <br />
           <li>
@@ -23,14 +23,14 @@ const Education = () => {
           <br />
           <li>2022</li>
         </div>
-        <div className={styles.educationContainer}>
+        <div className={`${styles.educationContainer} reveal`}>
           <h1>Fundação Bradesco</h1>
           <br />
           <li>Certificado - Inovando com CSS</li>
           <br />
           <li>2022</li>
         </div>
-        <div className={styles.educationContainer}>
+        <div className={`${styles.educationContainer} reveal`}>
           <h1>Fundação Bradesco</h1>
           <br />
           <li>Certificado - Introdução a JavaScript</li>
