ddbbf46095208e975d5c47dfab95fbf932b70fc4
Davi Jorge <guimell11@hotmail.com>
2023-02-17 00:55:38 -0300

    added animation logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import styles from "../styles/page.module.css";
 import Title from "components/title";
@@ -7,8 +8,29 @@ import Projects from "components/projects";
 import Contact from "components/contact";
 import Education from "components/education";
 import Experience from "components/experience";
+
+import { useEffect } from "react";
+import { inView } from "components/inView";
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
+  useEffect(() => {
+    // handle scroll in view animation
+    function handleScroll() {
+      inView({
+        elements: document.querySelectorAll(".reveal"),
+        elementVisibleThreshold: 100,
+        inViewFn: (e, i) => {
+          e.classList.add("revealShowing");
+        },
+      });
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <main className={styles.main}>
       <Title />
