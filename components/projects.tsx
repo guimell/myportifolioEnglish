b2039b0e33f81651aa309fc94eaaea759e078f8c
Davi Jorge <guimell11@hotmail.com>
2023-02-18 13:37:50 -0300

    added new project

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,6 +3,9 @@ import styles from "../src/styles/projects.module.css";
 import { Inter } from "@next/font/google";
 import movieMatterImage from "public/images/movieMatter.png";
 import atlas from "public/images/atlas.jpg";
+import zakkyCo from "public/images/zakky.png";
+import coPilot from "public/images/coPilot.png";
+import marmoraria from "public/images/marmoraria2.png";
 const inter = Inter({ subsets: ["latin"] });
 
 const Project = ({
@@ -28,6 +31,7 @@ const Project = ({
         </div>
         <div className={styles.flipCardBack}>
           <h1>{title}</h1>
+          <br />
           <p>{subTitle}</p>
           <br />
           <p>{description}</p>
@@ -42,6 +46,7 @@ const Projects = () => {
     <div id="portifolio" className="section">
       {/* my projects */}
       <h1 className="title">Projetos</h1>
+      <h1>Mobile</h1>
       <div className={`${styles.portifolio} reveal`}>
         <Project
           src={movieMatterImage}
@@ -70,6 +75,28 @@ const Projects = () => {
           description={` Um jogo de pixel art onde você, Atlas, defende sua casa de ondas
                 infinitas de inimigos. Você pode escolher um Cavaleiro, um Mago
                 ou um Arqueiro nesta missão para defender sua casa!`}
+        />
+      </div>
+      <h1>Web</h1>
+      <div className={`${styles.portifolio} reveal`}>
+        <Project
+          src={zakkyCo}
+          alt={"site da Zakky"}
+          title={"Zakky Company"}
+          description={"um website para aumentar o engajamento da sua empresa"}
+        />
+        <Project
+          src={coPilot}
+          alt={"site Co-Pilot"}
+          title={"Co-Pilot"}
+          subTitle={"[Em desenvolvimento]"}
+          description={"um website para achar e realizar serviços "}
+        />
+        <Project
+          src={marmoraria}
+          alt={"site da marmoraria santo antonio"}
+          title={"Marmoraria santo antonio"}
+          description={"web site feito para a empresa de marmores"}
         />
       </div>
     </div>
